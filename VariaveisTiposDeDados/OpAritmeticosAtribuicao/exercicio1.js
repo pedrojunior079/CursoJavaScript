77d16517b326b57aaa0b93afac1787d768c609d2
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-02-27 15:37:04 -0300

    Operadores Aritmeticos e Atribuição

---
+++ VariaveisTiposDeDados/OpAritmeticosAtribuicao/exercicio1.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 10;
+let soma = a + b; // 15
+let diferenca = a - b; // - 5
+let produto = a * b; // 50
+let divisao = b / a // 2
+let resto = b % a; // 0
+
+a++;     // a = 6
+b--;     // b = 9
+b -= 5;  // b = 4
+a *= 3;  // a = 33
+b /= 2;  // b = 2
+
+let resultado = 3 + 5 * 2; // 13, nao 16
+console.log(resultado);
---
+++ VariaveisTiposDeDados/OpAritmeticosAtribuicao/exercicio3.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 10;
+let c = 25;
+let soma = a + b + c; 
+let diferenca = a - b - c; 
+let produto = a * b * c; 
+let divisao = b / a / c; 
+let resto = b % a % c; 
+
+a++;     
+b--;
+c++;     
+b -= 5;
+c += 10;  
+a *= 3;  
+b /= 2;
+c /= 15;  
+
+let resultado = 3 + 5 * 2 + 10 / 15; 
+console.log(resultado);
